33760f7c079b198f833f918ecb2f377d78fc5332
Mariya Kosheleva <marevenc@gmail.com>
2020-09-22 20:49:21 +0300

    Added 1 LongAdder

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -1,18 +1,19 @@
-import java.util.Arrays;
 import java.util.concurrent.Callable;
 import java.util.concurrent.atomic.LongAdder;
 
 public class Account implements Callable {
-    int [] proceeds = new int[50];
+    Integer [] proceeds = new Integer[50];
 
-    public Account(){
+    LongAdder stat = new LongAdder();
+
+    public Account(LongAdder stat){
         for(int i = 0; i < proceeds.length; i++){
             proceeds[i] = (int) Math.round((Math.random() * 200));
+            stat.add(proceeds[i]);
         }
     }
 
-    public Integer call(){
-        LongAdder stat = new LongAdder();
-        return Arrays.stream(proceeds).sum();
+    public Integer[] call(){
+        return proceeds;
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,23 +1,23 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.*;
+import java.util.concurrent.atomic.LongAdder;
 
 public class Main {
-    public static void main(String[] args) throws InterruptedException, ExecutionException {
+    public static void main(String[] args) throws InterruptedException {
 
         int numberOfAccounts = 3;
+        LongAdder stat = new LongAdder();
 
         final ExecutorService threadPool = Executors.newFixedThreadPool(numberOfAccounts);
         List<Callable<Integer>> tasks = new ArrayList<>();
-        tasks.add(new Account());
-        tasks.add(new Account());
-        tasks.add(new Account());
+        tasks.add(new Account(stat));
+        tasks.add(new Account(stat));
+        tasks.add(new Account(stat));
 
-        int resultOfAll = 0;
-        for(Future<Integer> task : threadPool.invokeAll(tasks)){
-            resultOfAll += task.get();
-        }
-        System.out.println("Выручка по " + numberOfAccounts + " магазинам за день составила: " + resultOfAll);
+        List<Future<Integer>> futures = threadPool.invokeAll(tasks);
+
+        System.out.println("Выручка по " + numberOfAccounts + " магазинам за день составила: " + stat.sum());
         threadPool.shutdown();
     }
 }
