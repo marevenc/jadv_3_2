0d3fbadd562fed8d6db138bd7d50cbaad65a7599
Mariya Kosheleva <marevenc@gmail.com>
2020-09-24 21:11:59 +0300

    Cleared code

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -2,18 +2,18 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.atomic.LongAdder;
 
 public class Account implements Callable {
+    LongAdder stat;
     Integer [] proceeds = new Integer[50];
 
-    LongAdder stat = new LongAdder();
-
     public Account(LongAdder stat){
-        for(int i = 0; i < proceeds.length; i++){
-            proceeds[i] = (int) Math.round((Math.random() * 200));
-            stat.add(proceeds[i]);
-        }
+            this.stat = stat;
     }
 
     public Integer[] call(){
+        for(int i = 0; i < proceeds.length; i++) {
+            proceeds[i] = (int) Math.round((Math.random() * 200));
+            stat.add(proceeds[i]);
+        }
         return proceeds;
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,7 @@ import java.util.concurrent.*;
 import java.util.concurrent.atomic.LongAdder;
 
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
 
         int numberOfAccounts = 3;
         LongAdder stat = new LongAdder();
@@ -15,7 +15,7 @@ public class Main {
         tasks.add(new Account(stat));
         tasks.add(new Account(stat));
 
-        List<Future<Integer>> futures = threadPool.invokeAll(tasks);
+        threadPool.invokeAll(tasks);
 
         System.out.println("Выручка по " + numberOfAccounts + " магазинам за день составила: " + stat.sum());
         threadPool.shutdown();
